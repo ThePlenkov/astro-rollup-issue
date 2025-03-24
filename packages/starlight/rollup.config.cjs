285849b0bfc4a6aba1d0c4d70a1367b4853f75bc
Petr Plenkov <petr.plenkov@gmail.com>
2025-03-24 18:00:31 +0000

    here is the fix

--- packages/starlight/rollup.config.cjs
+++ packages/starlight/rollup.config.cjs
@@ -1,16 +1,51 @@
-const { withNx } = require('@nx/rollup/with-nx');
+const { withNx } = require("@nx/rollup/with-nx");
 
-module.exports = withNx(
+const config = withNx(
   {
-    main: './src/index.ts',
-    outputPath: './dist',
-    tsConfig: './tsconfig.lib.json',
-    compiler: 'swc',
+    main: "./src/index.ts",
+    outputPath: "./dist",
+    tsConfig: "./tsconfig.lib.json",
+    compiler: "swc",
     format: ["esm"],
+    external: "all",
+    sourceMap: true,
+    deleteOutputPath: true,
+    skipTypeCheck: true,
+    skipTypeField: true,
   },
   {
+    // plugins: [
+    //   typescript({
+    //     tsconfig: './tsconfig.lib.json',
+    //     useTsconfigDeclarationDir: true,
+    //     clean: true,
+    //     exclude: ['**/*.d.ts', 'node_modules'],
+    //     cacheRoot: './.rpt2_cache', // Use a custom cache directory
+    //     useCache: false, // Disable caching
+    //   }),
+    // ],
     // Provide additional rollup configuration here. See: https://rollupjs.org/configuration-options
     // e.g.
     // output: { sourcemap: true },
+    // external: (id) =>
+    //   ["@astrojs/starlight"].find((p) => {
+    //     if (id.includes(p)) {
+    //       console.log("External: ", id);
+    //       return true;
+    //     }
+    //   }),
   }
 );
+
+
+if (config.plugins) {
+  // delete rpt2 plugin
+  config.plugins = config.plugins.filter((plugin) => {
+    return plugin.name !== 'rpt2';
+  });
+}
+
+module.exports = config;
+
+
+// console.log("Rollup Config: ", JSON.stringify(module.exports, null, 2));
