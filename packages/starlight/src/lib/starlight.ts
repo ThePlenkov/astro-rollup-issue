eb6233d9d533ef0aaadf7c72fee24631767a246a
Petr Plenkov <petr.plenkov@gmail.com>
2025-03-24 16:36:28 +0000

    using starlight

--- packages/starlight/src/lib/starlight.ts
+++ packages/starlight/src/lib/starlight.ts
@@ -1,3 +1,20 @@
-export function starlight(): string {
-  return 'starlight';
+import type { docsSchema } from '@astrojs/starlight/schema';
+import { z } from 'astro/zod';
+
+type DocsSchema = ReturnType<typeof docsSchema>;
+type DocsSchemaType = z.infer<ReturnType<DocsSchema>>;
+
+export function starlight(): DocsSchemaType {
+  return {
+    title: 'test',
+    editUrl: '',
+    template: 'doc',
+    draft: false,
+    head: [],
+    pagefind: false,
+    sidebar: {
+      attrs: {},
+      hidden: false
+    }
+  }
 }
